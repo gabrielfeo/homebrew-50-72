5e19840576ffa446036c51a5ce94592c8daff3d1
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-27 03:31:34 +0100

    Create workflow

---
+++ update-formula.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+print(sys.argv)
+
+# with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+#     for line in file:
+#         print(line.replace(text_to_search, replacement_text), end='')
