2a0674239dc73a5289d5bbffb935e134befb3cef
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-27 04:00:07 +0100

    Write update script

--- update-formula.py
+++ update-formula.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
+from fileinput import FileInput
 
-print(sys.argv)
+FORMULA_PATH = 'Formula/fifty-seventy-two-cli.rb'
 
-# with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
-#     for line in file:
-#         print(line.replace(text_to_search, replacement_text), end='')
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', type=str, dest='version')
+parser.add_argument('--linux-sha', type=str, dest='linux_sha')
+parser.add_argument('--macos-sha', type=str, dest='macos_sha')
+args = parser.parse_args()
+
+with FileInput(FORMULA_PATH, inplace=True) as file:
+    for line in file:
+        if "version = " in line:
+            print(f"  version = \"{args.version}\"")
+        elif "linux_sha = " in line:
+            print(f"  linux_sha = \"{args.linux_sha}\"")
+        elif "macos_sha = " in line:
+            print(f"  macos_sha = \"{args.macos_sha}\"")
+        else:
+            print(line, end='')
